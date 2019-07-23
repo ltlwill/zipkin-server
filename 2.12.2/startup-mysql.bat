c42080508afa0e26670e29826a9f08962767a5bb
ltlwill <609738992@qq.com>
2019-07-23 18:07:45 +0800

    by ltl

--- 2.12.2/startup-mysql.bat
+++ 2.12.2/startup-mysql.bat
@@ -1,1 +1,1 @@
-java -DSTORAGE_TYPE=mysql -DMYSQL_DB=zipkin -DMYSQL_USER=root -DMYSQL_PASS=root -DMYSQL_HOST=localhost -DMYSQL_TCP_PORT=3306 -jar zipkin-server-2.12.2-exec.jar > startup-mysql.log --zipkin.collector.rabbitmq.uri=amqp://ms_client_test:test1234@192.168.2.6:5672 
+java -DSTORAGE_TYPE=mysql -DMYSQL_DB=zipkin -DMYSQL_USER=root -DMYSQL_PASS=root1234 -DMYSQL_HOST=localhost -DMYSQL_TCP_PORT=3306 -jar zipkin-server-2.12.2-exec.jar > startup-mysql.log --zipkin.collector.rabbitmq.uri=amqp://ms_client_test:test1234@192.168.2.6:5672 
