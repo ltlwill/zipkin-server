a3a1975714fd641159f7ea335b2f9ac5522dbcf2
ltlwill <609738992@qq.com>
2019-08-14 14:10:55 +0800

    by ltl

--- 2.15.0/startup-mysql.bat
+++
@@ -1,1 +0,0 @@
-java -DSTORAGE_TYPE=mysql -DMYSQL_DB=zipkin215 -DMYSQL_USER=db_ebay_dev -DMYSQL_PASS=db_ebay_devpwd -DMYSQL_HOST=192.168.2.6 -DMYSQL_TCP_PORT=3306 -jar zipkin-server-2.15.0-exec.jar > startup-mysql.log --zipkin.collector.rabbitmq.uri=amqp://ms_client_pro:pro!!1234@192.168.2.108:5672
